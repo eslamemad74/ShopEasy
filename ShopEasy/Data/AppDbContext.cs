38adf9db74174f77d4477e12001c253a053bdd42
eslam3mad <aslamalrys9@gmail.com>
2026-03-23 19:30:49 +0200

    Add Project Structure

---
+++ ShopEasy/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopEasy.Data
+{
+    public class AppDbContext : DbContext
+    {
+    }
+}
